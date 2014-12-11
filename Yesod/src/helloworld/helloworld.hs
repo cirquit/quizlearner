6b249f5efd94cba6069c0d770e84fb6e7f4f19f4
MujoA <mujo.alic@yahoo.com>
2014-12-11 20:47:14 +0100

    HelloWorld with hamlet and lucius (css)

--- Yesod/src/helloworld/helloworld.hs
+++ Yesod/src/helloworld/helloworld.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 import           Yesod
-
+import Import (widgetFile)
 data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
@@ -12,8 +12,16 @@ mkYesod "HelloWorld" [parseRoutes|
 
 instance Yesod HelloWorld
 
+test1 :: String 
+test1 = "JOHNY"
+
+test2 :: String 
+test2 = "GILBERT"
+
 getHomeR :: Handler Html
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout $ do
+	$(widgetFile "helloworld")
 
 main :: IO ()
 main = warp 3000 HelloWorld
+
