9c5a45c886ffda2725992260732e8df01430272c
cirquit <alex.isenko@googlemail.com>
2014-12-29 20:33:36 +0100

    trying input forms

--- Yesod/src/develTest/Handler/ExamOne.hs
+++ Yesod/src/develTest/Handler/ExamOne.hs
@@ -2,31 +2,44 @@ module Handler.ExamOne where
 
 import Import
 import TemporaryLibrary
+import Data.List ((!!))
 
 getExamOneR :: Handler Html
-getExamOneR = do
-    (widget, enctype) <- generateFormPost answer_form
-    defaultLayout $
-     $(widgetFile "exam_form")
-
+getExamOneR = do --(widget, enctype) <- generateFormPost $ answer_form
+                  defaultLayout $ do $(widgetFile "exam_form")
 
 postExamOneR :: Handler Html
 postExamOneR = do
-   ((result, widget), enctype) <- runFormPost answer_form
-   case result of
-       FormSuccess test_answers -> defaultLayout $
-             $(widgetFile "exam_answers")
-       _ -> defaultLayout $ [whamlet|
-                             <p> Error! You dun goofed...this should never happen.
-                             <a id=exam_title href=@{ExamOneR}> Get back!>
-                            |]
+        result <- runInputPost $ Returned_Answers
+                                 <$> ireq checkBoxField "1"
+                                 <*> ireq checkBoxField "2"
+                                 <*> ireq checkBoxField "3"
+                                 <*> ireq checkBoxField "4"
+                                 <*> ireq checkBoxField "5"
+                                 <*> ireq checkBoxField "6"
+                                 <*> ireq checkBoxField "7"
+                                 <*> ireq checkBoxField "8"
+                                 <*> ireq checkBoxField "9"
+                                 <*> ireq checkBoxField "10"
+                                 <*> ireq checkBoxField "11"
+                                 <*> ireq checkBoxField "12"
+        defaultLayout $ do $(widgetFile "exam_answers")
+--   ((result, widget), enctype) <- runFormPost $ answer_form
+--   case result of
+--       FormSuccess test_answers -> defaultLayout $
+--             $(widgetFile "exam_answers")
+--       _ -> defaultLayout $ [whamlet|
+--                             <p> Error! You dun goofed...this should never happen.
+--                             <a id=exam_title href=@{ExamOneR}> Get back!>
+--                            |]
 
-test_answer_AForm :: AForm Handler Test_Answer
-test_answer_AForm = Test_Answer
-  <$> areq checkBoxField "Testing...1" Nothing
-  <*> areq checkBoxField "Testing...2" Nothing
-  <*> areq checkBoxField "Testing...3" Nothing
-  <*> areq checkBoxField "Testing...4" Nothing
+--test_answer_AForm :: AForm Handler Test_Answer
+--test_answer_AForm = Test_Answer
+--  <$> areq checkBoxField "Nothing..." Nothing
+--  <*> areq checkBoxField "Nothing..." Nothing
+--  <*> areq checkBoxField "Nothing..." Nothing
+--  <*> areq checkBoxField "Nothing..." Nothing
 
-answer_form :: Form Test_Answer
-answer_form = renderTable test_answer_AForm
+
+--answer_form :: Form Test_Answer
+--answer_form = renderTable $ test_answer_AForm
--- Yesod/src/develTest/TemporaryLibrary.hs
+++ Yesod/src/develTest/TemporaryLibrary.hs
@@ -1,6 +1,7 @@
 module TemporaryLibrary where
 
 import Import
+import Data.List ((!!))
 
 
 data Test_Answer = Test_Answer
@@ -11,10 +12,10 @@ data Test_Answer = Test_Answer
     }
 
 instance Show Test_Answer where
-  show (Test_Answer a b c d) = "Box 1 = " ++ (show a) ++ "|" ++
-                               "Box 2 = " ++ (show b) ++ "|" ++
-                               "Box 3 = " ++ (show c) ++ "|" ++
-                               "Box 4 = " ++ (show d) ++ "|"
+  show (Test_Answer a b c d) = "Box 1 = " ++ (show a) ++ " | " ++
+                               "Box 2 = " ++ (show b) ++ " | " ++
+                               "Box 3 = " ++ (show c) ++ " | " ++
+                               "Box 4 = " ++ (show d) ++ " | "
 data Exam = Exam
     {   exam_title      :: Text
       , exam_max_score  :: Integer --in points
@@ -37,6 +38,20 @@ data Answer = Answer
      , is_checked      :: Bool
      , answer_hint     :: Text
     }
+
+data Returned_Answers = Returned_Answers {res1_1, res1_2, res1_3, res1_4,
+                                          res2_1, res2_2, res2_3, res2_4,
+                                          res3_1, res3_2, res3_3, res3_4 :: Bool}
+
+hip :: Bool -> String
+hip x = if x then "O" else "X"
+
+instance Show Returned_Answers where
+  show (Returned_Answers a b c d e f g h i j k l) = " | Q1 >> a1=" ++ hip a ++ " | a2= " ++ hip b ++ " | a3= " ++ hip c ++ " | a4=" ++ hip d ++
+                                                    " | Q2 >> a1=" ++ hip e ++ " | a2= " ++ hip f ++ " | a3= " ++ hip g ++ " | a4=" ++ hip h ++
+                                                    " | Q3 >> a1=" ++ hip i ++ " | a2= " ++ hip j ++ " | a3= " ++ hip k ++ " | a4=" ++ hip l
+
+
 
 -- Temporary Exams
 exams :: [Exam]
@@ -65,19 +80,35 @@ fst_qas :: [Answer]
 fst_qas = [fst_a1, fst_a2, fst_a3, fst_a4]
 
 snd_qas :: [Answer]
-snd_qas = [fst_a1, fst_a2, fst_a3, fst_a4]
+snd_qas = [snd_a1, snd_a2, snd_a3, snd_a4]
 
 thd_qas :: [Answer]
-thd_qas = [fst_a1, fst_a2, fst_a3, fst_a4]
+thd_qas = [trd_a1, trd_a2, trd_a3, trd_a4]
 
 fst_a1 :: Answer
-fst_a1 = Answer {answer_id = 00000001, answer_content="1", is_correct=False, answer_hint="This is hint", is_checked=False}
-
+fst_a1 = Answer {answer_id = 1, answer_content="1", is_correct=False, answer_hint="This is hint", is_checked=False}
 fst_a2 :: Answer
-fst_a2 = Answer {answer_id = 00000002, answer_content="2", is_correct=False, answer_hint="This is hint", is_checked=False}
-
+fst_a2 = Answer {answer_id = 2, answer_content="2", is_correct=False, answer_hint="This is hint", is_checked=False}
 fst_a3 :: Answer
-fst_a3 = Answer {answer_id = 00000003, answer_content="4", is_correct=False, answer_hint="This is hint", is_checked=False}
-
+fst_a3 = Answer {answer_id = 3, answer_content="4", is_correct=False, answer_hint="This is hint", is_checked=False}
 fst_a4 :: Answer
-fst_a4 = Answer {answer_id = 00000004, answer_content="5", is_correct=True, answer_hint="This is hint", is_checked=False}
+fst_a4 = Answer {answer_id = 4, answer_content="5", is_correct=True, answer_hint="This is hint", is_checked=False}
+
+snd_a1 :: Answer
+snd_a1 = Answer {answer_id = 5, answer_content="1", is_correct=False, answer_hint="This is hint", is_checked=False}
+snd_a2 :: Answer
+snd_a2 = Answer {answer_id = 6, answer_content="2", is_correct=False, answer_hint="This is hint", is_checked=False}
+snd_a3 :: Answer
+snd_a3 = Answer {answer_id = 7, answer_content="4", is_correct=False, answer_hint="This is hint", is_checked=False}
+snd_a4 :: Answer
+snd_a4 = Answer {answer_id = 8, answer_content="5", is_correct=True, answer_hint="This is hint", is_checked=False}
+
+trd_a1 :: Answer
+trd_a1 = Answer {answer_id = 9, answer_content="1", is_correct=False, answer_hint="This is hint", is_checked=False}
+trd_a2 :: Answer
+trd_a2 = Answer {answer_id = 10, answer_content="2", is_correct=False, answer_hint="This is hint", is_checked=False}
+trd_a3 :: Answer
+trd_a3 = Answer {answer_id = 11, answer_content="4", is_correct=False, answer_hint="This is hint", is_checked=False}
+trd_a4 :: Answer
+trd_a4 = Answer {answer_id = 12, answer_content="5", is_correct=True, answer_hint="This is hint", is_checked=False}
+
