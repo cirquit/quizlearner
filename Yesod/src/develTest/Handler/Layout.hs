b347ad2ebaa9165302f65fc1238281bde66311a0
cirquit <alex.isenko@googlemail.com>
2014-12-30 17:40:17 +0100

    testing modular widgets

--- Yesod/src/develTest/Handler/Layout.hs
+++ Yesod/src/develTest/Handler/Layout.hs
@@ -3,6 +3,18 @@ module Handler.Layout where
 import Import
 import TemporaryLibrary
 
+titleWidget :: Widget
+titleWidget = toWidget [hamlet| <div id=top class=front>
+                                    <a id=title href=@{LayoutR}> QuizLearner
+                       |]
+
+leftWidget :: Widget
+leftWidget = toWidget [hamlet| <ul id=exam_list>
+                                    <li> <a id=exam_title href=@{LayoutR}> Exams </a>
+                                    $forall single_exam <- exams
+                                        <li><a class=button href=@{ExamOneR}> #{exam_title single_exam} </a>
+ |]
+
 getLayoutR :: Handler Html
 getLayoutR = defaultLayout $ do
   setTitle "Basic Layout"
