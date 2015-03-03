7650d74245286106a74f4fcca3cb33851c4924fd
cirquit <alex.isenko@googlemail.com>
2015-03-03 14:48:03 +0100

    slight changes in quizcreator forms

--- quizlearner_/Handler/Quizcreator.hs
+++ quizlearner_/Handler/Quizcreator.hs
@@ -19,19 +19,19 @@ postQuizcreatorR = do
     ((res, _), _) <- runFormPost questionForm
     let middleWidget = case res of
          (FormSuccess (Question content list)) -> [whamlet|
-                                                   <span class=simpleWhite> #{show content}
+                                                   <p class=simpleWhite> #{content}
                                                       $forall (Answer content isCorrect) <- list
-                                                          <span class=simpleWhite> #{show content} is #{show isCorrect}
+                                                          <p class=simpleWhite> #{content} is #{show isCorrect}
                                                   |]
-         _                  -> [whamlet| <span class=simpleWhite> Something went wrong...|]
+         _                                     -> [whamlet| <span class=simpleWhite> Something went wrong...|]
     defaultLayout $ do $(widgetFile "quizcreator")
 
 questionForm :: Html -> MForm Handler ((FormResult Question), Widget)
 questionForm token = do
   let fourTimes = [1..4] :: [Int]
-  (qNameResult, qNameView) <- mreq textField "testing...1" Nothing
-  aTextFields <- forM fourTimes (\_ -> mreq textField "testing...2" Nothing)
-  aBoolFields <- forM fourTimes (\_ -> mreq boolField "testing...3" (Just False))
+  (qNameResult, qNameView) <- mreq textField "" Nothing
+  aTextFields <- forM fourTimes (\_ -> mreq textField "" Nothing)
+  aBoolFields <- forM fourTimes (\_ -> mreq boolField "" (Just False))
   let (aTextResults, aTextViews) = unzip aTextFields
   let (aBoolResults, aBoolViews) = unzip aBoolFields
   let answerList = FormSuccess $ zipWith (\(FormSuccess x) (FormSuccess y) -> Answer x y) aTextResults aBoolResults
@@ -40,10 +40,10 @@ questionForm token = do
   let widget = [whamlet|
                   #{token}
                           <ul class=questCreator>
-                            <li class=simpleWhite>Questiontext <span style="color:black"> ^{fvInput qNameView} </span>
-                            $forall (tview, bview, c) <- answerViews
-                                <li class=simpleWhite>Answer Nr.#{show c} <span style="color:black"> ^{fvInput tview} </span>
-                                                      Is it correct?      <span> ^{fvInput bview} </span>
+                              <li class=simpleWhite>Questiontext <span style="color:black"> ^{fvInput qNameView} </span>
+                              $forall (tview, bview, c) <- answerViews
+                                  <li class=simpleWhite>Answer Nr.#{show c} <span style="color:black"> ^{fvInput tview} </span>
+                                                        Is it correct?      <span> ^{fvInput bview} </span>
                         <input type=submit value="Submit question!">
                |]
   return (quest, widget)
